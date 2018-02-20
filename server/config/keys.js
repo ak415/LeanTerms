4e8379c18af7764a37ebcceda3f91b8ecd5a7ab0
Areej <areejht@gmail.com>
2018-02-19 19:45:48 -0800

    Delete keys.js

--- server/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  sessionSecret: 'kjlhfuhff23l23u394324'
-  // mongoURI:
-  //   // 'mongodb:;//areej:Phupert--101@ds139705.mlab.com:39705/react-node-course',
-  // cookieKey: 'khsdfkshiuhrkjnsdfuu3h48840u0u4t902u3r-03ujfsdo339r4'
-};
