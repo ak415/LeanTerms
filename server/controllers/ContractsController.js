ccc72d9952c912c086116da58f5eaab0b1827dc9
Areej Hassan <areejht@gmail.com>
2018-02-21 17:11:35 -0800

    add contracts controller file

---
+++ server/controllers/ContractsController.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Contract = require('../models/contract');
+
+// const showContract = (req, res, next) => {
+//   Contract.findById(re)
+// };
+
+module.exports = {};
