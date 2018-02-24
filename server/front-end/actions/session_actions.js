8510fab960225075c40257235249da6642ac7ae7
Ali Berenjian <Ali.rberenjian@gmail.com>
2018-02-23 18:23:38 -0800

    Fixed Navbar Login

--- server/front-end/actions/session_actions.js
+++ server/front-end/actions/session_actions.js
@@ -1,45 +1,60 @@
 import * as SessionAPI from '../utils/session_api';
 
-export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
-export const REMOVE_CURRENT_USER = 'REMOVE_CURRENT_USER';
-export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
-export const RECEIVE_SIGNUP_ERRORS = 'RECEIVE_SIGNUP_ERRORS';
 
-const ReceiveCurrentUser = user => ({
-  type: RECEIVE_CURRENT_USER,
-  user
-});
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const REMOVE_CURRENT_USER = "REMOVE_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+export const RECEIVE_SIGNUP_ERRORS = "RECEIVE_SIGNUP_ERRORS";
 
-const RemoveCurrentUser = () => ({
-  type: REMOVE_CURRENT_USER
-});
 
-const ReceiveErrors = errors => ({
-  type: RECEIVE_SESSION_ERRORS,
-  errors
-});
 
-export const LoginUser = user => dispatch =>
-  SessionAPI.LoginUser(user).then(
-    CurrentUser => dispatch(ReceiveCurrentUser(CurrentUser)),
-    err => dispatch(ReceiveErrors(err.responseJSON))
-  );
-
-export const LogoutUser = () => dispatch =>
-  SessionAPI.Logout().then(
-    () => dispatch(RemoveCurrentUser()),
-    err => dispatch(ReceiveErrors(err.responseJSON))
-  );
-
-export const CreateUser = user => dispatch =>
-  SessionAPI.CreateUser(user).then(
-    currentuser => dispatch(ReceiveCurrentUser(currentuser)),
-    err => dispatch(ReceiveErrors(err.responseJSON))
-  );
-
-export const CurrentUser = () => dispatch => (
-  SessionAPI.CurrentUser().then(user => dispatch(ReceiveCurrentUser(user))),
-  err => dispatch(ReceiveErrors(err.responseJSON))
+const ReceiveCurrentUser = (user) => (
+    {
+        type: RECEIVE_CURRENT_USER,
+        user
+    }
 );
 
-export const ClearError = () => dispatch => dispatch(ReceiveErrors([]));
+const RemoveCurrentUser = () =>(
+    {
+        type: REMOVE_CURRENT_USER,
+    }
+);
+
+const ReceiveErrors = errors =>(
+    {
+        type: RECEIVE_SESSION_ERRORS,
+        errors
+    }
+);
+
+
+export const LoginUser = (user)=> dispatch =>(
+    SessionAPI.LoginUser(user)
+        .then((CurrentUser) => dispatch(ReceiveCurrentUser(CurrentUser))
+            ,err => dispatch(ReceiveErrors(err.responseJSON)))
+);
+
+
+export const LogoutUser = () => dispatch =>(
+    SessionAPI.Logout()
+      .then(() => dispatch(RemoveCurrentUser()),
+          err => dispatch(ReceiveErrors(err.responseJSON)))
+);
+
+export const CreateUser = (user) =>dispatch =>(
+    SessionAPI.CreateUser(user)
+        .then((currentuser) => dispatch(ReceiveCurrentUser(currentuser)),
+        err => dispatch(ReceiveErrors(err.responseJSON)))
+);
+
+
+export const CurrentUser= () => dispatch =>(
+    SessionAPI.CurrentUser()
+        .then((user) => dispatch(ReceiveCurrentUser(user))),
+        err => dispatch(ReceiveErrors(err.responseJSON))
+);
+
+export const ClearError = () => dispatch =>(
+    dispatch(ReceiveErrors([]))
+); 
--- server/front-end/components/navbar.jsx
+++ server/front-end/components/navbar.jsx
@@ -78,205 +78,208 @@ class Navbar extends React.Component {
 
   render() {
     let display;
-    if (
-      this.props.currentUser &&
-      Object.keys(this.props.currentUser).length > 0
-    ) {
+    if ( this.props.currentUser && Object.keys(this.props.currentUser).length > 0) {
       display = (
         <button onClick={e => this.handlesubmitlogout(e)}>Logout</button>
       );
     } else {
-      display = '';
+      display =(
+          <div>
+              <div>
+                <div>
+                    <div className="main-nav">
+                      <button
+                        id="login-effects"
+                        className="cd-signup"
+                        onClick={() => this.changeDisplay('id02')}
+                      >
+                        Log In
+                      </button>
+
+                      <button
+                        id="nav-bar-signup"
+                        className="cd-signup"
+                        onClick={() => this.changeDisplay('id01')}
+                      >
+                        Sign Up
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                <div id="id01" className="modal">
+                  <form className="modal-content">
+                    <div className="container">
+                      <span
+                        onClick={() => this.changeDisplay('id01')}
+                        className="close"
+                        title="Close Modal"
+                      >
+                        X
+                      </span>
+                      <h1>Sign Up</h1>
+                      <p>Please fill in this form to create an account.</p>
+                      <hr />
+                      <label>
+                        <b>Username</b>
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Enter Username"
+                        name="email"
+                        onChange={this.update('username')}
+                        required
+                      />
+
+                      <label>
+                        <b>Email</b>
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Enter Email"
+                        name="psw"
+                        onChange={this.update('email')}
+                        required
+                      />
+
+                      <label>
+                        <b>Password</b>
+                      </label>
+                      <input
+                        type="password"
+                        placeholder="Enter Password"
+                        name="psw-repeat"
+                        onChange={this.update('password')}
+                        required
+                      />
+
+                      <div className="clearfix">
+                        <button
+                          type="submit"
+                          className="signup"
+                          onClick={e => this.handlesubmitnewuser(e)}
+                        >
+                          Sign Up
+                        </button>
+                        <button
+                          type="button"
+                          onClick={() => this.changeDisplay('id01')}
+                          className="cancelbtn"
+                        >
+                          Cancel
+                        </button>
+                      </div>
+
+                      <div className="form-last-line">
+                        <button type="button" className="demobtn">
+                          Demo
+                        </button>
+
+                        <p className="alternative-option">
+                          Already have an account?{' '}
+                          <a
+                            id="log-in-instead"
+                            href="#"
+                            onClick={this.switchToLogIn}
+                            style={{ color: '#c24e04d4' }}
+                          >
+                            Log In
+                          </a>.
+                        </p>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+
+                <div id="id02" className="modal">
+                  <form className="modal-content">
+                    <div className="container">
+                      <span
+                        onClick={() => this.changeDisplay('id02')}
+                        className="close"
+                        title="Close Modal"
+                      >
+                        X
+                      </span>
+                      <h1>Log In</h1>
+                      <p>
+                        In order to access your account, please enter your information.
+                      </p>
+                      <hr />
+                      <label>
+                        <b>Username</b>
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Enter Username"
+                        onChange={this.update('username')}
+                        required
+                      />
+
+                      <label>
+                        <b>Password</b>
+                      </label>
+                      <input
+                        type="password"
+                        placeholder="Enter Password"
+                        required
+                        onChange={this.update('password')}
+                      />
+
+                      <div className="clearfix">
+                        <button
+                          type="submit"
+                          className="signup"
+                          onClick={e => this.handlesubmitlogin(e)}
+                        >
+                          Log In
+                        </button>
+                        <button
+                          type="button"
+                          onClick={() => this.changeDisplay('id02')}
+                          className="cancelbtn"
+                        >
+                          Cancel
+                        </button>
+                      </div>
+
+                      <div className="form-last-line">
+                        <button type="button" className="demobtn">
+                          Demo
+                        </button>
+
+                        <p className="alternative-option">
+                          Don't have an account?{' '}
+                          <a
+                            id="sign-up-instead"
+                            href="#"
+                            onClick={this.switchToSignUp}
+                            style={{ color: '#c24e04d4' }}
+                          >
+                            Sign Up
+                          </a>.
+                        </p>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+          </div>
+      );
     }
 
     return (
       <div>
-        {display}
         <div className="wrap-nav-and-info">
           <div className="nav-bar">
             <div className="logo-title-wrap">
-              <img
-                src="http://res.cloudinary.com/aazaiez/image/upload/v1519269426/logo_3_LT_sknvf1.svg"
-                className="LeanTerms-logo-white"
-              />
-              <div>LeanTerms</div>
-            </div>
-
-            <div className="main-nav">
-              <button
-                id="login-effects"
-                className="cd-signup"
-                onClick={() => this.changeDisplay('id02')}
-              >
-                Log In
-              </button>
-
-              <button
-                id="nav-bar-signup"
-                className="cd-signup"
-                onClick={() => this.changeDisplay('id01')}
-              >
-                Sign Up
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div id="id01" className="modal">
-          <form className="modal-content">
-            <div className="container">
-              <span
-                onClick={() => this.changeDisplay('id01')}
-                className="close"
-                title="Close Modal"
-              >
-                X
-              </span>
-              <h1>Sign Up</h1>
-              <p>Please fill in this form to create an account.</p>
-              <hr />
-              <label>
-                <b>Username</b>
-              </label>
-              <input
-                type="text"
-                placeholder="Enter Username"
-                name="email"
-                onChange={this.update('username')}
-                required
-              />
-
-              <label>
-                <b>Email</b>
-              </label>
-              <input
-                type="text"
-                placeholder="Enter Email"
-                name="psw"
-                onChange={this.update('email')}
-                required
-              />
-
-              <label>
-                <b>Password</b>
-              </label>
-              <input
-                type="password"
-                placeholder="Enter Password"
-                name="psw-repeat"
-                onChange={this.update('password')}
-                required
-              />
-
-              <div className="clearfix">
-                <button
-                  type="submit"
-                  className="signup"
-                  onClick={e => this.handlesubmitnewuser(e)}
-                >
-                  Sign Up
-                </button>
-                <button
-                  type="button"
-                  onClick={() => this.changeDisplay('id01')}
-                  className="cancelbtn"
-                >
-                  Cancel
-                </button>
-              </div>
-
-              <div className="form-last-line">
-                <button type="button" className="demobtn">
-                  Demo
-                </button>
-
-                <p className="alternative-option">
-                  Already have an account?{' '}
-                  <a
-                    id="log-in-instead"
-                    href="#"
-                    onClick={this.switchToLogIn}
-                    style={{ color: '#c24e04d4' }}
-                  >
-                    Log In
-                  </a>.
-                </p>
+                  <img
+                    src="http://res.cloudinary.com/aazaiez/image/upload/v1519269426/logo_3_LT_sknvf1.svg"
+                    className="LeanTerms-logo-white"
+                  />
+                  <div>LeanTerms</div>
               </div>
             </div>
-          </form>
-        </div>
-
-        <div id="id02" className="modal">
-          <form className="modal-content">
-            <div className="container">
-              <span
-                onClick={() => this.changeDisplay('id02')}
-                className="close"
-                title="Close Modal"
-              >
-                X
-              </span>
-              <h1>Log In</h1>
-              <p>
-                In order to access your account, please enter your information.
-              </p>
-              <hr />
-              <label>
-                <b>Username</b>
-              </label>
-              <input
-                type="text"
-                placeholder="Enter Username"
-                onChange={this.update('username')}
-                required
-              />
-
-              <label>
-                <b>Password</b>
-              </label>
-              <input
-                type="password"
-                placeholder="Enter Password"
-                required
-                onChange={this.update('password')}
-              />
-
-              <div className="clearfix">
-                <button
-                  type="submit"
-                  className="signup"
-                  onClick={e => this.handlesubmitlogin(e)}
-                >
-                  Log In
-                </button>
-                <button
-                  type="button"
-                  onClick={() => this.changeDisplay('id02')}
-                  className="cancelbtn"
-                >
-                  Cancel
-                </button>
-              </div>
-
-              <div className="form-last-line">
-                <button type="button" className="demobtn">
-                  Demo
-                </button>
-
-                <p className="alternative-option">
-                  Don't have an account?{' '}
-                  <a
-                    id="sign-up-instead"
-                    href="#"
-                    onClick={this.switchToSignUp}
-                    style={{ color: '#c24e04d4' }}
-                  >
-                    Sign Up
-                  </a>.
-                </p>
-              </div>
-            </div>
-          </form>
+            {display}
         </div>
       </div>
     );
